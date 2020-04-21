9a92dce1d90df21b3fabb37a50491f443e5e035f
Afham Siddiqui <62937387+Afham-Siddiqui@users.noreply.github.com>
2020-04-21 16:24:02 +0500

    Simple Hangman game

--- Hangman game.py
+++ Hangman game.py
@@ -93,8 +93,8 @@ while turns > 0:
         print("You have", + turns, 'more guesses')
 
         if turns == 0:
-            print("You Loose! You didnot have save your friend.
-\n"
+            print("You Loose! You didnot have save your friend.\n
+
                   "--------\n"
                   "|       \n"
                   "|       \n"
